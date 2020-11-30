29c88f8507fb93bcfe5e6f0c19933bd6ce4557dc
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2020-11-30 15:38:05 +0700

    Mengubah fragmen sejarah,kuliner,penginapan menjadi activity

--- Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/HomeFragment.kt
+++ Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/HomeFragment.kt
@@ -1,12 +1,13 @@
 package com.alfa_sc1.mastur
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.synnapps.carouselview.ImageListener
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_home.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,8 +21,10 @@ private const val ARG_PARAM2 = "param2"
  */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
@@ -37,29 +40,17 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val fragmentSejarah = SejarahFragment()
         btn_sejarah.setOnClickListener {
-            fragmentManager?.beginTransaction()?.apply {
-                replace(R.id.container, fragmentSejarah, SejarahFragment::class.java.simpleName)
-                    .addToBackStack(null)
-                    .commit()
-            }
+            val mIntent = Intent(activity, SejarahActivity::class.java)
+            startActivity(mIntent)
         }
-        val fragmentKuliner = KulinerFragment()
         btn_kuliner.setOnClickListener {
-            fragmentManager?.beginTransaction()?.apply {
-                replace(R.id.container, fragmentKuliner, KulinerFragment::class.java.simpleName)
-                        .addToBackStack(null)
-                        .commit()
-            }
+            val mIntent = Intent(activity, KulinerActivity::class.java)
+            startActivity(mIntent)
         }
-        val fragmentPenginapan = PenginapanFragment()
         btn_penginapan.setOnClickListener {
-            fragmentManager?.beginTransaction()?.apply {
-                replace(R.id.container, fragmentPenginapan, PenginapanFragment::class.java.simpleName)
-                        .addToBackStack(null)
-                        .commit()
-            }
+            val mIntent = Intent(activity, PenginapanActivity::class.java)
+            startActivity(mIntent)
         }
     }
 
---
+++ Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/PenginapanActivity.kt
@@ -0,0 +1,11 @@
+package com.alfa_sc1.mastur
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PenginapanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_penginapan)
+    }
+}
