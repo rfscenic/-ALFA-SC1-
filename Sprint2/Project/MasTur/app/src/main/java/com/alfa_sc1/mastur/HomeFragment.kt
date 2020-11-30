2a032a02cf912f5549797c882f8b8ed14226f461
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2020-11-30 11:27:03 +0700

    connect button

--- Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/HomeFragment.kt
+++ Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.synnapps.carouselview.ImageListener
 import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -31,6 +32,34 @@ class HomeFragment : Fragment() {
             val args = Bundle()
             fragment.arguments = args
             return fragment
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentSejarah = SejarahFragment()
+        btn_sejarah.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.container, fragmentSejarah, SejarahFragment::class.java.simpleName)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+        val fragmentKuliner = KulinerFragment()
+        btn_kuliner.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.container, fragmentKuliner, KulinerFragment::class.java.simpleName)
+                        .addToBackStack(null)
+                        .commit()
+            }
+        }
+        val fragmentPenginapan = PenginapanFragment()
+        btn_penginapan.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.container, fragmentPenginapan, PenginapanFragment::class.java.simpleName)
+                        .addToBackStack(null)
+                        .commit()
+            }
         }
     }
 
