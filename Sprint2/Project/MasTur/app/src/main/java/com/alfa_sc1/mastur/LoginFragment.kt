825c7cee1451a9429ddf284a120a9ee11f341672
abdulhp <hpabdul902@gmail.com>
2021-01-27 09:58:16 +0700

    Login with Email and Password + Firebase Auth Configuration

--- Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/LoginFragment.kt
+++ Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/LoginFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.alfa_sc1.mastur.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_login.*
 
@@ -19,14 +22,15 @@ private const val ARG_PARAM2 = "param2"
  * Use the [LoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class LoginFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
@@ -38,11 +42,38 @@ class LoginFragment : Fragment() {
         }
     }
 
+    private lateinit var auth: FirebaseAuth
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        button2.setOnClickListener {
+
+        binding.btnFrgLogin.setOnClickListener(this)
+        binding.btnEmail.setOnClickListener(this)
+        binding.btnPhone.setOnClickListener(this)
+
+        btn_frg_login.setOnClickListener {
             val mIntent = Intent(activity, MainActivity::class.java)
             startActivity(mIntent)
         }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_frg_login -> {
+                signIn(binding.inputFrgEmail.text.toString(), binding.inputFrgPassword.text.toString())
+            }
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    activity?.finish()
+                    val intent = Intent(activity, MainActivity::class.java)
+                    startActivity(intent)
+                }else {
+                    Toast.makeText(activity?.baseContext, "Authentication Faled", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
