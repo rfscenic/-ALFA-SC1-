a347ae41a3fd980266c4d26ddc38f1f45814555c
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-01-27 23:14:48 +0700

    Fix Login Fragment Bug

--- Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/LoginFragment.kt
+++ Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/LoginFragment.kt
@@ -2,14 +2,17 @@ package com.alfa_sc1.mastur
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil.setContentView
 import com.alfa_sc1.mastur.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.fragment_home.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_login.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -46,14 +49,16 @@ class LoginFragment : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         binding.btnFrgLogin.setOnClickListener(this)
         binding.btnEmail.setOnClickListener(this)
         binding.btnPhone.setOnClickListener(this)
 
-        btn_frg_login.setOnClickListener {
-            val mIntent = Intent(activity, MainActivity::class.java)
-            startActivity(mIntent)
-        }
+        auth = Firebase.auth
+//        btn_frg_login.setOnClickListener {
+//            val mIntent = Intent(activity, MainActivity::class.java)
+//            startActivity(mIntent)
+//        }
     }
 
     override fun onClick(v: View?) {
@@ -65,6 +70,9 @@ class LoginFragment : Fragment(), View.OnClickListener {
     }
 
     private fun signIn(email: String, password: String) {
+        if (!validateForm()) {
+            return
+        }
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener {
                 if (it.isSuccessful) {
@@ -75,5 +83,23 @@ class LoginFragment : Fragment(), View.OnClickListener {
                     Toast.makeText(activity?.baseContext, "Authentication Faled", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+    private fun validateForm(): Boolean {
+        var valid = true
+        val email = binding.inputFrgEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            binding.inputFrgEmail.error = "Required."
+            valid = false
+        } else {
+            binding.inputFrgEmail.error = null
+        }
+        val password = binding.inputFrgPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.inputFrgPassword.error = "Required."
+            valid = false
+        } else {
+            binding.inputFrgPassword.error = null
+        }
+        return valid
     }
 }
