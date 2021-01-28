70ab1292d85e4d78670d0bb32578a32430546114
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-01-28 11:22:29 +0700

    Manampilkan nama, email dan foto akun pada profil

--- Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/ProfilFragment.kt
+++ Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/ProfilFragment.kt
@@ -1,9 +1,18 @@
 package com.alfa_sc1.mastur
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.*
 import androidx.fragment.app.Fragment
+import com.alfa_sc1.mastur.databinding.FragmentProfilBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,10 +25,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ProfilFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ProfilFragment : Fragment() {
+class ProfilFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: FragmentProfilBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +40,71 @@ class ProfilFragment : Fragment() {
         }
         setHasOptionsMenu(true)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            currentUser.let { updateUI(it) }
+        }
+        else {
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnLogOut.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnLogOut -> {
+                signOut()
+            }
+        }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    @SuppressLint("SetTextI18n")
+    private fun updateUI(currentUser: FirebaseUser) {
+        currentUser.let {
+            val name = currentUser.displayName
+            val phoneNumber = currentUser.phoneNumber
+            val email = currentUser.email
+            val photoUrl = currentUser.photoUrl
+            val emailVerified = currentUser.isEmailVerified
+            val uid = currentUser.uid
+            binding.tvName.text = name
+            if(TextUtils.isEmpty(name)){
+                binding.tvName.text = "No Name"
+            }
+            binding.tvEmail.text = email
+            if(photoUrl !== null){
+                Glide.with(this)
+                    .load(photoUrl)
+                    .apply(RequestOptions().override(200, 200))
+                    .into(binding.ivImage)
+            }
+    //            for (profile in it.providerData) {
+    //                val providerId = profile.providerId
+    //                if(providerId=="password" && emailVerified==true){
+    //                    binding.btnEmailVerify.isVisible = false
+    //                }
+    //                if(providerId=="phone"){
+    //                    binding.tvName.text = phoneNumber
+    //                    binding.tvUserId.text = providerId
+    //                }
+    //            }
+        }
+    }
+
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_profil, menu)
     }
@@ -47,9 +123,10 @@ class ProfilFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false)
+        binding = FragmentProfilBinding.inflate(inflater, container, false)
+        return binding.root
     }
     companion object {
         /**
@@ -71,3 +148,6 @@ class ProfilFragment : Fragment() {
             }
     }
 }
+
+
+
