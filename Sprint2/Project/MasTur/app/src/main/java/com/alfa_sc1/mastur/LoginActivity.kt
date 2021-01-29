3226b62da1d08f52ec08962b439c488bbdba53da
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-01-29 08:36:37 +0700

    added BroadcastReceiver to Listen to Internet Connection

--- Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/LoginActivity.kt
+++ Sprint2/Project/MasTur/app/src/main/java/com/alfa_sc1/mastur/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.alfa_sc1.mastur
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import com.alfa_sc1.mastur.adapter.LoginActivityViewPagerAdapter
 import com.google.android.material.tabs.TabLayout
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), ConnectivityReceiver.ConnectivityReceiverListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -16,5 +19,22 @@ class LoginActivity : AppCompatActivity() {
 
         mViewPager.adapter = LoginActivityViewPagerAdapter(supportFragmentManager)
         mTabLayout.setupWithViewPager(mViewPager)
+        registerReceiver(ConnectivityReceiver(), IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+    override fun onNetworkConnectionChanged(isConnected: Boolean) {
+        showNetworkMessage(isConnected)
+    }
+    override fun onResume() {
+        super.onResume()
+        ConnectivityReceiver.connectivityReceiverListener = this
+    }
+    private fun showNetworkMessage(isConnected: Boolean) {
+        if (!isConnected) {
+            Toast.makeText(this, "You are offline",
+                Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "You are online",
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
